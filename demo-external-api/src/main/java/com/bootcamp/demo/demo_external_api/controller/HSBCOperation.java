98122c6b590d8e51a3b68ab7146879bb34693276
ShayneTS <shaynelee10@gmail.com>
2026-01-24 17:22:15 +0800

    update

--- demo-external-api/src/main/java/com/bootcamp/demo/demo_external_api/controller/HSBCOperation.java
+++ demo-external-api/src/main/java/com/bootcamp/demo/demo_external_api/controller/HSBCOperation.java
@@ -2,12 +2,12 @@ package com.bootcamp.demo.demo_external_api.controller;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.bootcamp.demo.demo_external_api.dto.UserDto;
 import com.bootcamp.demo.demo_external_api.model.Cat;
-import com.bootcamp.demo.demo_external_api.model.dto.UserDTO;
 
 public interface HSBCOperation {
   @GetMapping(value = "/hsbc/users")
-  List<UserDTO> getUsers();
+  List<UserDto> getUsers();
 
   @GetMapping(value = "/hsbc/cat")
   Cat getCat();
